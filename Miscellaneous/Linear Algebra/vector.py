0be4cd768be3891db03303438f39f091bb66a56b
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-11-01 20:29:20 -0500

    Implemented vector subtraction and scalar multiplication modules

--- Miscellaneous/Linear Algebra/vector.py
+++ Miscellaneous/Linear Algebra/vector.py
@@ -39,6 +39,28 @@ class Vector(object):
         return Vector(new_coordinates)
 
 
+    def minus(self, v):
+        """Subtracts two vectors and returns their difference vector"""
+
+        new_coordinates = [x-y for x, y in zip(self.coordinates, v.coordinates)]
+
+        # new_coordinates = []
+        # n = len(self.coordinates)
+        # for i in range(n):
+        #    new_coordinates.append(self.coordinates[i] - v.coordinates[i])
+
+        return Vector(new_coordinates)
+
+
+    def times_scalar(self, c):
+        """Returns the product of a vector and a scalar"""
+
+        new_coordinates = [ c*x for x in self.coordinates]
+        return Vector(new_coordinates)
+
+
+
+
 
 
 my_vector = Vector([1,2,3])
@@ -48,3 +70,5 @@ a = Vector([1, 5, 6])
 b = Vector([5, 8, 10])
 
 print a.plus(b)
+print a.minus(b)
+print a.times_scalar(3)
